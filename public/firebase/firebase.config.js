3891b895f3469ca9f14a2a1d461b1ca16c222b3c
unknown <evan.doict@gmail.com>
2024-10-21 14:39:08 +0600

    env

--- public/firebase/firebase.config.js
+++ public/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDZE81wbhLS2YmkMThyaJ-7eT2hff4JIMA",
-  authDomain: "assignment5-70e97.firebaseapp.com",
-  projectId: "assignment5-70e97",
-  storageBucket: "assignment5-70e97.appspot.com",
-  messagingSenderId: "845798478239",
-  appId: "1:845798478239:web:f6b8114e971d16f4416daa"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKUP,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
